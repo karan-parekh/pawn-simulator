2fa2a8b224df50c12de3944ab3d5989f31974dec
Karan Parekh <karanparekh501@gmail.com>
2025-12-09 15:43:12 +1100

    build: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="flex flex-col items-center">Pawn Simulator</h1>
+    </div>
+  )
+}
+
+export default App
